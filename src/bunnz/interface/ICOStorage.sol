bbf8aa00b1604057712a248ab6ed1159a93799d4
dunnie99 <oluwadunnirachael@gmail.com>
2023-04-14 04:30:21 +0100

    ICO update

--- src/bunnz/interface/ICOStorage.sol
+++ src/bunnz/interface/ICOStorage.sol
@@ -4,4 +4,19 @@ pragma solidity ^0.8.0;
 contract ICOStorage {
     address public Token;
     uint256 public priceForOneToken;
+
+    uint256 projectStartTime;
+    uint256 projectStopTime;
+
+    address projectOwner;
+    string projectName;
+    string projectDescription;
+    address tokenContractAddress;
+    uint256 totalShare;
+    uint256 value;
+
+    address[] tokenHolders;
+    mapping(address => bool) isTokenHolder;
+    mapping(address => uint256) share;
+
 }
